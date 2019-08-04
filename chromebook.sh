891e5371f7945ec95b2c1bfb90b6ea63775ab989
Ben Reaves <rbreaves@gmail.com>
2019-08-04 03:34:36 -0500

    - Enabled mac word-wise feature for chromebook based installs.

--- chromebook.sh
+++ chromebook.sh
@@ -4,4 +4,4 @@ cp ./.xkb/symbols/chromebook ~/.xkb/symbols/chromebook
 setxkbmap -option
 setxkbmap -print > ~/.xkb/keymap/kbd.gui
 line=$(cat ~/.xkb/keymap/kbd.gui | grep -n 'xkb_symbols' | cut -f1 -d:)
-sed -ie "${line}s/)\"/)+chromebook(swap_lalt_lctrl)\"/g" ~/.xkb/keymap/kbd.gui
+sed -ie "${line}s/)\"/)+chromebook(swap_lalt_lctrl)+mac_onelvl(mac_onelvlsym)\"/g" ~/.xkb/keymap/kbd.gui
