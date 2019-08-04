bd4cbe291cda29638a55a2d4afebc7824ecebb00
Ben Reaves <rbreaves@gmail.com>
2019-08-03 21:44:39 -0500

    - Corrected directory name that stores local xkb files

--- chromebook.sh
+++ chromebook.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-mkdir -p ~/.kbd/{keymap,symbols}
+mkdir -p ~/.xkb/{keymap,symbols}
 cp ./.xkb/symbols/chromebook ~/.xkb/symbols/chromebook
 setxkbmap -option
 setxkbmap -print > ~/.xkb/keymap/kbd.gui
 line=$(cat ~/.xkb/keymap/kbd.gui | grep -n 'xkb_symbols' | cut -f1 -d:)
 sed -ie "${line}s/)\"/)+chromebook(swap_lalt_lctrl)\"/g" ~/.xkb/keymap/kbd.gui
-#xkbcomp -w0 -I$HOME/.xkb ~/.xkb/keymap/kbd.gui $DISPLAY
