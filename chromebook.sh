d4dbfd67dca6cea0a732bdcdb5a5c64f4bea1ede
Ben Reaves <rbreaves@gmail.com>
2019-08-05 15:34:14 -0500

    - Corrected keyboard detection bug and updated files in prep of a future keymap update.

--- chromebook.sh
+++ chromebook.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
-mkdir -p ~/.xkb/{keymap,symbols}
+mkdir -p ~/.xkb/{keymap,symbols,types}
 cp ./.xkb/symbols/chromebook ~/.xkb/symbols/chromebook
+cp ./.xkb/symbols/mac_gui ~/.xkb/symbols/mac_gui
+cp ./.xkb/types/mac_term ~/.xkb/types/mac_term
 setxkbmap -option
-setxkbmap -print > ~/.xkb/keymap/kbd.gui
-line=$(cat ~/.xkb/keymap/kbd.gui | grep -n 'xkb_symbols' | cut -f1 -d:)
-sed -ie "${line}s/)\"/)+chromebook(swap_lalt_lctrl)+mac_onelvl(mac_onelvlsym)\"/g" ~/.xkb/keymap/kbd.gui
+setxkbmap -print > ~/.xkb/keymap/kbd.chromebook.gui
+line=$(cat ~/.xkb/keymap/kbd.chromebook.gui | grep -n 'xkb_symbols' | cut -f1 -d:)
+sed -ie "${line}s/)\"/)+chromebook(swap_lalt_lctrl)+mac_gui(mac_onelvlsym)\"/g" ~/.xkb/keymap/kbd.chromebook.gui
+sleep 1
+line=$(cat ~/.xkb/keymap/kbd.mac.gui | grep -n 'xkb_types' | cut -f1 -d:)
+sed -ie "${line}s/)\"/)+mac_gui(addmac_levels)\"/g" ~/.xkb/keymap/kbd.chromebook.gui
