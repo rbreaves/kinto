3c5b49324c2d39e2d4cc03f2bfbb0c75faa7c24b
Ben Reaves <rbreaves@gmail.com>
2020-02-05 04:09:20 -0600

    - Removed the creation of the autostart folder

--- keyswap_service.sh
+++ keyswap_service.sh
@@ -6,7 +6,6 @@ systemctl --user stop keyswap.timer >/dev/null 2>&1
 systemctl --user disable keyswap.timer >/dev/null 2>&1
 swapcmd="\/bin\/bash\ \/home\/`whoami`\/.config\/kinto\/xactive.sh"
 mkdir -p ~/.config/systemd/user
-mkdir -p ~/.config/autostart
 cp ./system-config/keyswap.service ~/.config/systemd/user/keyswap.service
 cp ./kintox11/binary/kintox11 ~/.config/kinto/kintox11
 cp ./system-config/xactive.sh ~/.config/kinto/xactive.sh
