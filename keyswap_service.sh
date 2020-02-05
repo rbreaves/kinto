b8b81bd907b5f7212ca210e659e285b0fbc3390b
Ben Reaves <rbreaves@gmail.com>
2020-02-05 05:24:40 -0600

    - Added desktop startup of kinto service

--- keyswap_service.sh
+++ keyswap_service.sh
@@ -6,7 +6,9 @@ systemctl --user stop keyswap.timer >/dev/null 2>&1
 systemctl --user disable keyswap.timer >/dev/null 2>&1
 swapcmd="\/bin\/bash\ \/home\/`whoami`\/.config\/kinto\/xactive.sh"
 mkdir -p ~/.config/systemd/user
+mkdir -p ~/.config/autostart
 cp ./system-config/keyswap.service ~/.config/systemd/user/keyswap.service
+cp ./system-config/kinto.desktop ~/.config/autostart/kinto.desktop
 cp ./kintox11/binary/kintox11 ~/.config/kinto/kintox11
 cp ./system-config/xactive.sh ~/.config/kinto/xactive.sh
 sed -i "s/{username}/`whoami`/g" ~/.config/systemd/user/keyswap.service
