6230c189faaf68df37a3637175085e468bbd0665
Ben Reaves <rbreaves@gmail.com>
2020-04-26 18:21:33 -0500

    - Changed inotify for xkeysnail config to monitor mode

--- xkeysnail-config/xkeystart.sh
+++ xkeysnail-config/xkeystart.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 # >/dev/null 2>&1
 /usr/local/bin/xkeysnail --quiet --watch "$1" &
-while true
-	do inotifywait -e close_write,moved_to,create -q "$1"
-		/usr/bin/killall xkeysnail
-		/usr/local/bin/xkeysnail --quiet --watch "$1" &
+
+inotifywait -m -e close_write,moved_to,create -q "$1" |
+while read -r path; do
+	/usr/bin/killall xkeysnail
+	/usr/local/bin/xkeysnail --quiet --watch "$1" &
 done
