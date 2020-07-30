0c5a9775ec8ae5b9ebf44ebdf5fae81c10a4c473
Ben Reaves <rbreaves@gmail.com>
2020-07-30 02:30:02 -0500

    - Prepping for Ubuntu Budgie system tray

--- xkeysnail-config/logoff.sh
+++ xkeysnail-config/logoff.sh
@@ -1,6 +1,7 @@
+#!/bin/bash
 dbus-monitor --session "type='signal',interface='org.gnome.SessionManager.Logout'" | grep '1' |
 while read x; do
-  echo "$x"
+  # echo "$x"
   sudo systemctl stop xkeysnail
-  break
+  exit 0
 done
