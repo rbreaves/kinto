02aa99f3015ee7b882625661879f304cece6d7fa
Ben Reaves <rbreaves@gmail.com>
2020-12-12 12:11:46 -0600

    - Removed all unused files and references

--- references/old-files/old_uninstall.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-systemctl --user stop keyswap
-systemctl --user disable keyswap
-rm -rf ~/.config/autostart/keyswap.sh
-rm -rf ~/.config/xactive.sh
-rm -rf ~/.xkb
-
-sed -i '/xkb/d' ~/.Xsession
