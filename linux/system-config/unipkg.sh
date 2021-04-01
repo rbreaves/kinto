ea01cafd5c547d32f14f1e81563147ebfc692afc
RedBearAK <64876997+RedBearAK@users.noreply.github.com>
2021-04-01 00:01:05 -0800

    Reverting back to master to remove from PR

--- linux/system-config/unipkg.sh
+++ linux/system-config/unipkg.sh
@@ -1,31 +1,15 @@
 #!/bin/bash
 
 if pkgmgr="$( which apt-get )" 2> /dev/null; then
-   echo 
-   echo "Debian apt-get package manager detected... "
-   echo "Installing $1... "
-   echo 
+   echo "Debian"
    $pkgmgr update
-   $pkgmgr --yes --allow-downgrades --allow-remove-essential --allow-change-held-packages install $1
+   $pkgmgr --yes --force-yes install $1
 elif pkgmgr="$( which dnf )" 2> /dev/null; then
-   echo 
-   echo "dnf package manager detected... "
-   echo "Installing $1... "
-   echo 
+   echo "dnf"
    $pkgmgr check-update; $pkgmgr install -y $1
 elif pkgmgr="$( which pacman )" 2> /dev/null; then
-   echo 
-   echo "Arch-based pacman package manager detected... "
-   echo "Installing $1... "
-   echo 
+   echo "Arch-based"
    $pkgmgr -Syy;yes | $pkgmgr -S $1
-elif pkgmgr="$( which zypper )" 2> /dev/null; then
-   echo 
-   echo "zypper package manager detected... "
-   echo "Installing $1... "
-   echo 
-   $pkgmgr refresh
-   $pkgmgr -n install $1
 else
    echo "Package manager not found, please install $1" >&2
    exit 1
