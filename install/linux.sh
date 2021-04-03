fbc630f1d509c3129d13a693e92ab9e0f1bc0c33
Ben Reaves <rbreaves@gmail.com>
2021-04-03 02:25:30 -0500

    Updated quick install to pull in release & build correctly

--- install/linux.sh
+++ install/linux.sh
@@ -6,6 +6,12 @@ curl https://github.com/rbreaves/kinto/archive/refs/heads/master.zip -J -L -o ~/
 unzip ~/Downloads/kinto.zip -d ~/Downloads/
 cd ~/Downloads/kinto-master/
 
+kintorelease=`curl -s https://api.github.com/repos/rbreaves/kinto/releases/latest | awk -F'tag_name": ' '{if ($2) print $2}' | tr -d \",`
+kintohash=`unzip -z ~/Downloads/kinto.zip | tail -n1`
+kintoshort=${kintohash::7}
+
+echo "$kintorelease" "build" "$kintoshort" > ./dl_version
+
 if [ $# -eq 0 ];then
 	echo "Installing Kinto..."
 	./setup.py
--- xkeysnail_service.sh
+++ xkeysnail_service.sh
@@ -377,7 +377,16 @@ yes | cp -rf ./linux/xkeysnail.desktop ~/.config/kinto/xkeysnail.desktop
 # logoff fix - not solid for every os. Prevents missed 1 character input on login
 # yes | sudo cp -rf linux/gnome_logoff.sh ~/.config/kinto/logoff.sh
 
-echo "$(git describe --tag --abbrev=0 | head -n 1)" "build" "$(git rev-parse --short HEAD)" > ~/.config/kinto/version
+if [ -d "./.git" ] 
+then
+	echo "$(git describe --tag --abbrev=0 | head -n 1)" "build" "$(git rev-parse --short HEAD)" > ~/.config/kinto/version
+elif [ -f "./dl_version" ]; then
+	cp ./dl_version  ~/.config/kinto/version
+else
+	# Not a typo - v is built in
+	echo "ersion Unknown"  > ~/.config/kinto/version
+fi
+
 yes | cp -rf ./linux/kinto.py ./linux/kinto.py.new
 yes | cp -rf ./linux/limitedadmins ./linux/limitedadmins.new
 yes | cp -rf ./linux/gui/ ~/.config/kinto/
