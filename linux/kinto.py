dcbf1e83825403af944415aa4ed2c1e50a6be809
Ben Reaves <rbreaves@gmail.com>
2021-07-05 00:26:36 -0500

    Added Remmina name variation for Ubuntu 18.04, improves remote access functionality

--- linux/kinto.py
+++ linux/kinto.py
@@ -44,6 +44,7 @@ codeStr = "|".join(str('^'+x+'$') for x in mscodes)
 remotes = [
     "Gnome-boxes",
     "org.remmina.Remmina",
+    "remmina",
     "qemu-system-.*",
     "Virt-manager",
     "VirtualBox",
